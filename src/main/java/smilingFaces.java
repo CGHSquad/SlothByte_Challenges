ddc06ef0f4363aaa0d147b1e95d01590cda02d02
realcgh <chrisghrobot@gmail.com>
2024-08-10 23:54:45 -0400

    Solution for smilingFaces.java

--- src/main/java/smilingFaces.java
+++ src/main/java/smilingFaces.java
@@ -19,12 +19,29 @@ public class smilingFaces {
     }
     public static int countSmileysV2(String[] smiles){
         //Runtime of O(n) as we only iterate through the array once
-        return 0;
+        int i = 0, count = 0;
+        while(i < smiles.length){
+            if(smiles[i].length() == 2){
+                if(smiles[i].charAt(1) == 'D' || smiles[i].charAt(1) == ')'){
+                    count++;
+                }
+            }else {
+                    if(smiles[i].charAt(2) == 'D' || smiles[i].charAt(2) == ')'){
+                        count++;
+                    }
+            }
+            i++;
+        }
+        return count;
     }
 
     public static void main(String[] args) {
         System.out.println(countSmileys(new String[] {":)", ";(", ";}", ":-D"}));
         System.out.println(countSmileys(new String[] {";D", ":-(", ":-)", ";~)"}));
         System.out.println(countSmileys(new String[] {";]", ":[", ";*", ":$", ";-D"}));
+
+        System.out.println(countSmileysV2(new String[] {":)", ";(", ";}", ":-D"}));
+        System.out.println(countSmileysV2(new String[] {";D", ":-(", ":-)", ";~)"}));
+        System.out.println(countSmileysV2(new String[] {";]", ":[", ";*", ":$", ";-D"}));
     }
 }
